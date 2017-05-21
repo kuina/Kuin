05ecbae3e3963f92de7808ade48a4bf755485292
kuina <kuinatesso@gmail.com>
2017-05-21 23:26:08 +0900

    Delete 'editor.c' and 'editor.h'.

--- src/compiler/editor.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "..\common.h"
-
-EXPORT void EditorInit(SClass* me_, void* func_ins, void* func_cmd, void* func_replace, void* func_undo_mark, SClass* scroll_x, SClass* scroll_y);
-EXPORT void EditorFin(void);
-EXPORT void EditorSetSrc(const void* src);
-EXPORT void EditorSetCursor(S64* newX, S64* newY, S64 x, S64 y, Bool refresh);
