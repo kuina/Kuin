499a6bafbed095a41e7141401bb2574d5740e718
kuina <kuinatesso@gmail.com>
2018-02-17 19:35:09 +0900

    A trivial change.

--- src/compiler/main.c
+++ src/compiler/main.c
@@ -252,7 +252,7 @@ EXPORT Bool Interpret2(const U8* path, const void*(*func_get_src)(const U8*), co
 EXPORT void Version(S64* major, S64* minor, S64* micro)
 {
 	*major = 2018;
-	*minor = 1;
+	*minor = 2;
 	*micro = 17;
 }
 
