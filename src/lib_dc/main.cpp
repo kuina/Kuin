6e03ffc5aab7396c1dbb60191d670ef1c6c80dd6
haruo-wakakusa <haruowakakusa@gmail.com>
2018-11-10 18:14:41 +0900

    ジオメトリ追加

--- src/lib_dc/main.cpp
+++ src/lib_dc/main.cpp
@@ -34,6 +34,10 @@ BOOL WINAPI DllMain(HINSTANCE hinst, DWORD reason, LPVOID reserved)
 	return TRUE;
 }
 
+//
+// ID2D1RenderTargetおよびそのサブクラス
+//
+
 struct SDeviceContext
 {
 	SClass Class;
@@ -52,6 +56,32 @@ struct SGdiDeviceContext
 	ID2D1DCRenderTarget* renderTarget;
 	HWND hwnd;
 };
+
+//
+// ID2D1Geomeotryおよびそのサブクラス
+//
+
+struct SGeometry
+{
+	SClass Class;
+	ID2D1Geometry* geometry;
+};
+
+struct SPathGeometry
+{
+	SGeometry parent;
+	ID2D1PathGeometry* geometry;
+};
+
+struct SGeometrySink
+{
+	SClass Class;
+	ID2D1GeometrySink* geometrySink;
+};
+
+//
+// ID2D1RenderTargetのコンストラクタ・デストラクタ・メソッド
+//
 
 EXPORT_CPP SClass* _makeWndDC(SClass* me_, S64 hwnd)
 {
@@ -190,6 +220,16 @@ EXPORT_CPP void _circleLine(
 	brush->Release();
 }
 
+EXPORT_CPP void _geometry(SClass* me_, SClass* geometry_, S64 color)
+{
+	SDeviceContext* me2 = reinterpret_cast<SDeviceContext*>(me_);
+	SGeometry* geometry = reinterpret_cast<SGeometry*>(geometry_);
+	ID2D1SolidColorBrush *brush;
+	me2->renderTarget->CreateSolidColorBrush(colorFromS64(color), &brush);
+	me2->renderTarget->FillGeometry(geometry->geometry, brush);
+	brush->Release();
+}
+
 EXPORT_CPP void _tri(
 	SClass* me_, double x1, double y1, double x2, double y2, double x3, double y3, S64 color)
 {
@@ -213,6 +253,92 @@ EXPORT_CPP void _tri(
 	geometry->Release();
 	sink->Release();
 }
+
+//
+// ID2D1Geometryのコンストラクタ・デストラクタ・メソッド
+//
+
+EXPORT_CPP SClass* _makePathGeometry(SClass *me_)
+{
+	SPathGeometry* me2 = reinterpret_cast<SPathGeometry*>(me_);
+	factory->CreatePathGeometry(&me2->geometry);
+	me2->geometry->QueryInterface(__uuidof(ID2D1Geometry), (LPVOID*)(&me2->parent.geometry));
+	return me_;
+}
+
+EXPORT_CPP void _pathGeometryDtor(SClass *me_)
+{
+	SPathGeometry* me2 = reinterpret_cast<SPathGeometry*>(me_);
+	me2->parent.geometry->Release();
+	me2->geometry->Release();
+}
+
+EXPORT_CPP void _geometrySinkDtor(SClass *me_)
+{
+	SGeometrySink* me2 = reinterpret_cast<SGeometrySink*>(me_);
+	me2->geometrySink->Release();
+}
+
+EXPORT_CPP SClass* _pathGeometryOpen(SClass *me_, SClass *me2)
+{
+	SPathGeometry* me3 = reinterpret_cast<SPathGeometry*>(me_);
+	SGeometrySink* me4 = reinterpret_cast<SGeometrySink*>(me2);
+	me3->geometry->Open(&me4->geometrySink);
+	return me2;
+}
+
+EXPORT_CPP void _geometrySinkBeginFigure(SClass* me_, double x, double y, S64 figureBegin)
+{
+	SGeometrySink* me2 = reinterpret_cast<SGeometrySink*>(me_);
+	me2->geometrySink->BeginFigure(D2D1::Point2F(x, y), (D2D1_FIGURE_BEGIN)figureBegin);
+}
+
+EXPORT_CPP void _geometrySinkEndFigure(SClass* me_, S64 figureEnd)
+{
+	SGeometrySink* me2 = reinterpret_cast<SGeometrySink*>(me_);
+	me2->geometrySink->EndFigure((D2D1_FIGURE_END)figureEnd);
+}
+
+EXPORT_CPP void _geometrySinkClose(SClass *me_)
+{
+	SGeometrySink* me2 = reinterpret_cast<SGeometrySink*>(me_);
+	me2->geometrySink->Close();
+}
+
+EXPORT_CPP void _geometrySinkAddArc(
+	SClass *me_, double x, double y, double rx, double ry, double angle,
+	S64 sweepDirection, S64 arcSize)
+{
+	SGeometrySink* me2 = reinterpret_cast<SGeometrySink*>(me_);
+	me2->geometrySink->AddArc(
+		D2D1::ArcSegment(
+			D2D1::Point2F(x, y),
+			D2D1::SizeF(rx, ry),
+			angle,
+			(D2D1_SWEEP_DIRECTION)sweepDirection,
+			(D2D1_ARC_SIZE)arcSize));
+}
+
+EXPORT_CPP void _geometrySinkAddBezier(
+	SClass *me_, double cx1, double cy1, double cx2, double cy2, double x, double y)
+{
+	SGeometrySink* me2 = reinterpret_cast<SGeometrySink*>(me_);
+	me2->geometrySink->AddBezier(
+		D2D1::BezierSegment(
+			D2D1::Point2F(cx1, cy1),
+			D2D1::Point2F(cx2, cy2),
+			D2D1::Point2F(x, y)));
+}
+
+EXPORT_CPP void _geometrySinkAddLine(SClass *me_, double x, double y)
+{
+	SGeometrySink* me2 = reinterpret_cast<SGeometrySink*>(me_);
+	me2->geometrySink->AddLine(D2D1::Point2F(x, y));
+}
+
+//
+// ヘルパー関数
+//
 
 static inline D2D1::ColorF colorFromS64(S64 color) {
 	union {
