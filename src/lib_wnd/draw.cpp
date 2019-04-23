394e0c17713a86eaf02f81fe41b8df34470a9af8
kuina <kuinatesso@gmail.com>
2019-04-23 19:00:50 +0900

    A trivial change.

--- src/lib_wnd/draw.cpp
+++ src/lib_wnd/draw.cpp
@@ -1102,7 +1102,7 @@ EXPORT_CPP SClass* _makeFont(SClass* me_, const U8* fontName, S64 size, bool bol
 		me2->Height = (double)tm.tmHeight;
 		SelectObject(me2->Dc, old_font);
 	}
-	me2->CellSizeAligned = 32; // Texture length must not be less than 128.
+	me2->CellSizeAligned = 128; // Texture length must not be less than 128.
 	while (me2->CellSizeAligned < me2->CellWidth + 1 || me2->CellSizeAligned < me2->CellHeight + 1)
 		me2->CellSizeAligned *= 2;
 	{
