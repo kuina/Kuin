ac99605da07113847b19c83a5e22aacff84dcf8d
kuina <kuinatesso@gmail.com>
2019-09-14 10:37:54 +0900

    A trivial change.

--- src/lib_game/main.h
+++ src/lib_game/main.h
@@ -6,6 +6,8 @@
 EXPORT void _init(void* heap, S64* heap_cnt, S64 app_code, const U8* use_res_flags);
 EXPORT void _rectMove(SClass* me_, double max_velo);
 EXPORT void _rectUpdate(SClass* me_);
+EXPORT void _circleMove(SClass* me_, double max_velo);
+EXPORT void _circleUpdate(SClass* me_);
 EXPORT SClass* _makeMapImpl(SClass* me_, S64 map_width, S64 map_height, const void* data, double chip_width, double chip_height);
 EXPORT SClass* _makeMapEmpty(SClass* me_, S64 map_width, S64 map_height, double chip_width, double chip_height);
 EXPORT void _mapDtor(SClass* me_);
